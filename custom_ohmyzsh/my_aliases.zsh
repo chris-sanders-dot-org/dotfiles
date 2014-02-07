52d79538b251fa3fce5e278f64c1105c3a82a668
Chris Sanders <christopherfsanders@gmail.com>
2014-02-07 11:17:21 -0500

    added my ohmyzsh customizations

---
+++ custom_ohmyzsh/my_aliases.zsh
@@ -0,0 +1,9 @@
+# my aliases
+alias l='ls -lh'
+alias la='ls -lah'
+alias lrt='ls -lhrt'
+alias cls='clear'
+
+alias sagi='sudo apt-get install'
+
+fliptable() { echo "（╯°□°）╯ ┻━┻"; } # Flip a table. Example usage: fsck -y /dev/sdb1 || fliptable
---
+++ custom_ohmyzsh/my_toggles.zsh
@@ -0,0 +1,2 @@
+# Uncomment following line if you want to disable command autocorrection
+DISABLE_CORRECTION="true"
